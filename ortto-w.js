6817d87f687dc9dc85ebbb4697cab18beed45041
Maciej J <maciej@autopilothq.com>
2022-11-22 10:52:02 +1100

    Update ortto-w.js

--- ortto-w.js
+++ ortto-w.js
@@ -5,8 +5,8 @@ self.addEventListener('push', function (event) {
 	console.log('[Service Worker] Push Received.');
 	console.log('[Service Worker] Push had this data:', event.data.text());
 
-	var title = event.data.json().title;
-	var options = event.data.json();
+	var title = event.data.text();
+	var options = event.data;
 
 	event.waitUntil(self.registration.showNotification(title, options));
 });
