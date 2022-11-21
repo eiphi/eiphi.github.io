2b033c176e9353e1b3c3ea85392a144303bf7834
Maciej J <maciej@autopilothq.com>
2022-11-22 10:47:50 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-/* eslint-disable */
-'use strict';
-
-self.addEventListener('push', function (event) {
-	console.log('[Service Worker] Push Received.');
-	console.log('[Service Worker] Push had this data:', event.data.text());
-
-	var title = event.data.text();
-	var options = event.data;
-
-	event.waitUntil(self.registration.showNotification(title, options));
-});
+importScripts('./ortto-w.js');
