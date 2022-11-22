6261eeb8f4f1fc56d2c0b69495d06439e3c18c43
Maciej J <maciej@autopilothq.com>
2022-11-23 10:54:35 +1100

    Update temp-script.js

--- temp-script.js
+++ temp-script.js
@@ -4,7 +4,6 @@
 self.addEventListener('push', function (event) {
 	console.log('[Service Worker] Push Received.');
 	console.log('[Service Worker] Push had this data:', event.data.text());
-	console.log('Using temp script');
 
 	var title = event.data.json().title;
 	var options = event.data.json();
@@ -12,20 +11,20 @@ self.addEventListener('push', function (event) {
 	event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', function  (event)  {
-  console.log('On notification click: ', event.notification.tag);
-  event.notification.close();
-
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then((clientList) => {
-    for (const client of clientList) {
-      if (client.url === '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
+self.addEventListener('notificationclick', function (event) {
+	event.notification.close();
+	// This looks to see if the current site is already open and focuses if it is
+	event.waitUntil(
+		clients
+			.matchAll({
+				type: 'window'
+			})
+			.then(function (clientList) {
+				for (var i = 0; i < clientList.length; i++) {
+					var client = clientList[i];
+					if (client.url === '/' && 'focus' in client) return client.focus();
+				}
+				if (clients.openWindow) return clients.openWindow('/');
+			})
+	);
 });
