f17b041999d3a529e8c06f6896ee3fa314462e7b
Maciej J <maciej@autopilothq.com>
2022-11-22 16:36:37 +1100

    Update temp-script.js

--- temp-script.js
+++ temp-script.js
@@ -11,3 +11,21 @@ self.addEventListener('push', function (event) {
 
 	event.waitUntil(self.registration.showNotification(title, options));
 });
+
+self.addEventListener('notificationclick', function  (event)  {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then((clientList) => {
+    for (const client of clientList) {
+      if (client.url === '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+});
