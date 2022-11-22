5415171e3aee28c8c527fc77d8b76f9af5231235
Maciej J <maciej@autopilothq.com>
2022-11-22 16:08:08 +1100

    Update temp-script.js

--- temp-script.js
+++ temp-script.js
@@ -4,6 +4,7 @@
 self.addEventListener('push', function (event) {
 	console.log('[Service Worker] Push Received.');
 	console.log('[Service Worker] Push had this data:', event.data.text());
+	console.log('Using temp script');
 
 	var title = event.data.json().title;
 	var options = event.data.json();
