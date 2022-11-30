8223e1ae1909e59a4ade66af65d4a41538eed06b
Maciej J <maciej@autopilothq.com>
2022-11-30 15:44:39 +1100

    Update temp-script.js

--- temp-script.js
+++ temp-script.js
@@ -11,11 +11,42 @@ self.addEventListener('push', function (event) {
 	event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', function (event) {
-	event.notification.close();
+function openUrl(_event) {
+	_event.waitUntil(
+		clients
+			.matchAll({
+				type: 'window'
+			})
+			.then(function (clientList) {
+				for (var i = 0; i < clientList.length; i++) {
+					var client = clientList[i];
+					if (client.url === '/' && 'focus' in client) return client.focus();
+				}
+				if (clients.openWindow) return clients.openWindow('/');
+			})
+	);
+}
+
+function handleActionClick(_actionData, _event) {
 	// Checks primary action for url and opens the window in client
-	var primaryActionData = event.notification.data && event.notification.data.primary_action;
 	if (primaryActionData && primaryActionData.url && primaryActionData.action === 'page') {
 		if (clients.openWindow) return clients.openWindow(primaryActionData.url);
 	}
+	if (primaryActionData && primaryActionData.action === 'talk') {
+		// TODO handle talk
+	}
+}
+
+self.addEventListener('notificationclick', function (event) {
+	var notificationData = event.notification.data;
+	if (event.action) {
+		// clicked an action
+		var secondaryActionData = notificationData && notificationData[event.action];
+		handleActionClick(secondaryActionData, event);
+	} else {
+		// clicked a notification
+		var primaryActionData = notificationData && notificationData.primary_action;
+		handleActionClick(primaryActionData, event);
+	}
+	event.notification.close();
 });
