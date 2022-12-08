cb0846f7a4e39c7b12bf13b185c04cc52e5eaf5d
Maciej J <maciej@autopilothq.com>
2022-12-08 12:08:58 +1100

    Update temp-script.js

--- temp-script.js
+++ temp-script.js
@@ -1,16 +1,6 @@
 /* eslint-disable */
 'use strict';
 
-self.addEventListener('push', function (event) {
-	console.log('[Service Worker] Push Received.');
-	console.log('[Service Worker] Push had this data:', event.data.text());
-
-	var title = event.data.json().title;
-	var options = event.data.json();
-
-	event.waitUntil(self.registration.showNotification(title, options));
-});
-
 function openUrl(_event, _url) {
 	_event.waitUntil(
 		clients
@@ -43,6 +33,20 @@ function handleActionClick(_event, _actionData) {
 		// TODO handle talk
 	}
 }
+
+self.addEventListener('install', function () {
+	self.skipWaiting();
+});
+
+self.addEventListener('push', function (event) {
+	console.log('[Service Worker] Push Received.');
+	console.log('[Service Worker] Push had this data:', event.data.text());
+
+	var title = event.data.json().title;
+	var options = event.data.json();
+
+	event.waitUntil(self.registration.showNotification(title, options));
+});
 
 self.addEventListener('notificationclick', function (event) {
 	var notificationData = event.notification.data;
