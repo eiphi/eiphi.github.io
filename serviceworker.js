d6006bef5cdee7669e0f709c9dbac5c728664ebb
Maciej J <maciej@autopilothq.com>
2023-02-22 14:17:45 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,2 @@
-importScripts("https://static.ap3stg.com/capture/master/push-service-worker-script.js");
+importScripts("./temp-script.js");
+// importScripts("https://static.ap3stg.com/capture/master/push-service-worker-script.js");
